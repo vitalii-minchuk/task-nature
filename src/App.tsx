6e4697b68e18a12025062ec1e5aa50051fc8a15f
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-09-08 11:13:29 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <label htmlFor="count">
+        <input
+          type="text"
+          className=""
+          style={{ width: '100px', height: '200px', marginRight: '20px' }}
+        />
+      </label>
+    </div>
+  );
+}
+
+export default App;
